24c1d63fa16b4a486b34133940faeb75c3116aac
Ahmed Mezghani <amezghani5@gmail.com>
2026-02-16 15:08:34 +0100

    Remove 'NewsletterSubscribed' column

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,3 +5,8 @@ print(df.info())
 print(df.isnull().sum())
 print(sorted(df["Age"].unique())) 
 print((df["SupportTicketsCount"].unique()))
+df = df.drop(columns=["NewsletterSubscribed"])
+print("-------------------")
+try: 
+    print((df["NewsletterSubscribed"].unique()))
+except : print("Column 'NewsletterSubscribed' has been dropped and is no longer available.")
