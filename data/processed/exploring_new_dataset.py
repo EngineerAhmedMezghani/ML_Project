99aee8e48aeddd6f800f89961fea34a45eaa1605
Ahmed Mezghani <amezghani5@gmail.com>
2026-05-05 19:59:31 +0100

    pushing the project to the ML repo

--- data/processed/exploring_new_dataset.py
+++ data/processed/exploring_new_dataset.py
@@ -4,36 +4,7 @@ df = pd.read_csv("data/processed/retail_customers_processed.csv")
 print(df.head())
 
 
+for col in df:
+    print(f"{col}: {sum(df[col].isnull())}")
 
-print(df["Gender_encoded"].unique())
-print(df["Gender_encoded"].value_counts())
-
-print(df["Region_encoded"].unique())
-print("Region_encoded value counts:",df["Region_encoded"].value_counts())
-
-print(df["FavoriteSeason_encoded"].unique())
-print("FavoriteSeason_encoded value counts:",df["FavoriteSeason_encoded"].value_counts())
-
-print(df["PreferredTimeOfDay_encoded"].unique())
-print("PreferredTimeOfDay_encoded value counts:",df["PreferredTimeOfDay_encoded"].value_counts())
-
-print(df["WeekendPreference_encoded"].unique())
-print("WeekendPreference_encoded value counts:",df["WeekendPreference_encoded"].value_counts())
-
-print(df["CustomerType_encoded"].unique())
-print("CustomerType_encoded value counts:",df["CustomerType_encoded"].value_counts())
-
-label_cols = [
-    "LoyaltyLevel",
-    "AgeCategory",
-    "SpendingCategory",
-    "ChurnRiskCategory",
-    "BasketSizeCategory",
-    "ProductDiversity",
-    "RFMSegment"
-]
-for col in label_cols:
-    print(df[col].unique())
-    print(f"{col} value counts:",df[col].value_counts())
-    print("---")
-
+print(df.shape)
